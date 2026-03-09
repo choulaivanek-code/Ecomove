53085a628b769515d36c7b522aea71905db5fe28
ivane <tonEmailGuthub@example.com>
2026-03-09 02:43:23 +0100

    add JaCoCo + SonarCloud pipeline2

--- src/test/java/com/example/ecomove/EcomoveApplicationTests.java
+++ src/test/java/com/example/ecomove/EcomoveApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.ecomove;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:mysql://localhost:3306/ecomove?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
+        "spring.datasource.username=root",
+        "spring.datasource.password=root",
+        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver"
+})
 class EcomoveApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
